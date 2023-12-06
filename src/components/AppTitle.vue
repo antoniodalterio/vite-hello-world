3b5fe5c4dc9572054fa3c4a81ff5a6a40895d43b
antoniodalterio <antoniodalterio2227@gmail.com>
2023-12-06 14:46:37 +0100

    feat: milestone

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'titolo',
+  data() {
+    return {
+      title: 'La mia prima app con Vite',
+    };
+  },
+};
+</script>
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scope>
+h1 {
+  font-size: 3.125rem;
+}
+</style>
